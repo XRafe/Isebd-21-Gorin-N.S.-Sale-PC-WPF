70bf9fb2d5f6e070e0ed0041dd11529fec24fb5f
XRafe <nikita-gorin98@yandex.ru>
2019-06-02 15:17:15 +0400

    finish

---
+++ SalePCView/WindowStock.xaml.cs
@@ -0,0 +1,96 @@
+﻿using SalePCServiceDAL.BindingModels;
+using SalePCServiceDAL.Interfaces;
+using SalePCServiceDAL.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using Unity;
+
+namespace SalePCView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowStock.xaml
+    /// </summary>
+    public partial class WindowStock : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public int Id { set { id = value; } }
+
+        private readonly IStockService service;
+
+        private int? id;
+
+        public WindowStock(IStockService service)
+        {
+            InitializeComponent();
+            Loaded += WindowStock_Load;
+            this.service = service;
+        }
+
+        private void WindowStock_Load(object sender, EventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    StockViewModel view = service.GetElement(id.Value);
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.StockName;
+                        dataGridViewStock.ItemsSource = view.StockHardware;
+                        dataGridViewStock.Columns[0].Visibility = Visibility.Hidden;
+                        dataGridViewStock.Columns[1].Visibility = Visibility.Hidden;
+                        dataGridViewStock.Columns[2].Visibility = Visibility.Hidden;
+                        dataGridViewStock.Columns[3].Width = DataGridLength.Auto;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                if (id.HasValue)
+                {
+                    service.UpdElement(new StockBindingModel
+                    {
+                        Id = id.Value,
+                        StockName = textBoxName.Text
+                    });
+                }
+                else
+                {
+                    service.AddElement(new StockBindingModel
+                    {
+                        StockName = textBoxName.Text
+                    });
+                }
+                MessageBox.Show("Сохранение прошло успешно", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ SalePCView/WindowStocks.xaml.cs
@@ -0,0 +1,97 @@
+﻿using SalePCServiceDAL.Interfaces;
+using SalePCServiceDAL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Unity;
+
+namespace SalePCView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowStocks.xaml
+    /// </summary>
+    public partial class WindowStocks : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        private readonly IStockService service;
+
+        public WindowStocks(IStockService service)
+        {
+            InitializeComponent();
+            Loaded += WindowStocks_Load;
+            this.service = service;
+        }
+
+        private void WindowStocks_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<StockViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridViewStocks.ItemsSource = list;
+                    dataGridViewStocks.Columns[0].Visibility = Visibility.Hidden;
+                    dataGridViewStocks.Columns[1].Width = DataGridLength.Auto;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<WindowStock>();
+            if (form.ShowDialog() == true)
+                LoadData();
+        }
+
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewStocks.SelectedItem != null)
+            {
+                var form = Container.Resolve<WindowStock>();
+                form.Id = ((StockViewModel)dataGridViewStocks.SelectedItem).Id;
+                if (form.ShowDialog() == true)
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewStocks.SelectedItem != null)
+            {
+                if (MessageBox.Show("Удалить запись?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    int id = ((StockViewModel)dataGridViewStocks.SelectedItem).Id;
+                    try
+                    {
+                        service.DelElement(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
